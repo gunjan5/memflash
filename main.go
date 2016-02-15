766147f7faf156ab0e503b715694911cde3e5fec
Gunjan Patel <patelgunjan5@gmail.com>
2016-02-15 02:11:52 -0800

    test

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+
+
+import (
+	"io"
+	"net/http"
+)
+
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
